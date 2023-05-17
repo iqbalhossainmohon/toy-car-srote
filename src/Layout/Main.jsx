313ffb84779d1aaaa640ab61b5935ff07edb4f39
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-05-17 23:02:48 +0600

    responsive nav bar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../Shared/NavBar/NavBar";
 
 
 const Main = () => {
     return (
         <div>
+            <NavBar/>
             <Outlet/>
         </div>
     );
