214a602616b53e17dd716ac8bc0abdc0008592d6
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-05-19 12:28:22 +0600

    data sent from addtoy form to mongodb server

--- src/Pages/AddAToys/AddAToys.jsx
+++ src/Pages/AddAToys/AddAToys.jsx
@@ -1,9 +1,23 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 const AddAToys = () => {
+    const { user } = useContext(AuthContext);
 
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
+    const onSubmit = (data) => {
+        fetch('http://localhost:5000/addToys', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(result => {
+            console.log(result);
+        })
         console.log(data)
     };
 
@@ -35,14 +49,37 @@ const AddAToys = () => {
                         />
                         <input
                             className="h-12 w-1/4 px-3 py-2 border border-purple-400 rounded-md "
-                            {...register("quantity")}
+                            {...register("quantity", { required: true })}
                             placeholder="quantity"
                         />
                     </div>
+
+                    <div className="my-2">
+                        <input
+                            className="h-12 w-1/4 px-3 py-2 border border-purple-400 rounded-md mr-4"
+                            value={user?.email}
+                            {...register("postedBy", { required: true })}
+                            placeholder="your email"
+                            type="email"
+                        />
+                        <input
+                            className="h-12 w-1/4 px-3 py-2 border border-purple-400 rounded-md "
+                            {...register("date", { required: true })}
+                            placeholder="date"
+                            type="date"
+                        />
+                    </div>
+
                     <input
                         className="h-12 w-2/4 px-3 py-2 border border-purple-400 rounded-md my-2"
                         {...register("photoURL")}
                         placeholder="photo url"
+                    />
+                    <br />
+                    <input
+                        className="h-20 w-2/4 px-3 py-2 border border-purple-400 rounded-md my-2"
+                        {...register("description")}
+                        placeholder="description"
                     />
                     <br />
                     <input className="submit-btn btn btn-primary mb-8 w-2/4" value="Add Toys" type="submit" />
